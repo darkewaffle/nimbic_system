035fde0847915401f750e834154ae11a6e3a15f0
darkwaffle <darkewaffle@gmail.com>
2024-10-29 21:27:50 -0400

    Proc definition update for WriteInPlace and BICtoJSON WriteInPlace logic

--- bic_json_conversion.nim
+++ bic_json_conversion.nim
@@ -1,14 +1,14 @@
-import std/[strutils, algorithm, streams, json, tables, os]
+import std/[strutils, algorithm, streams, json, tables, os, paths]
 import neverwinterdotnim/neverwinter/[gff, gffjson]
 import io_operations
 import nwn_gff_excerpts
 
 
-proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool)
-proc JSONtoBIC*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool)
+proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool = false, ConfigWriteInPlace: bool = false)
+proc JSONtoBIC*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool = false, ConfigWriteInPlace: bool = false)
 
 
-proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool) =
+proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool = false, ConfigWriteInPlace: bool = false) =
   echo "Attempting translation to JSON: " & InputFile
   #Streams data from InputFile as a string
   var InputStream = newStringStream(readFile(InputFile))
@@ -17,8 +17,15 @@ proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool) 
   var InputAsGFF: GffRoot
   InputAsGFF = InputStream.readGffRoot(false)
 
+  var WriteToDirectory: string
+  if ConfigWriteInPlace:
+    var SplitPath = splitFile(Path InputFile)
+    WriteToDirectory = $SplitPath.dir
+  else:
+    WriteToDirectory = OutputDirectory
+
   if ExpectSqlite:
-    var OutputPathSQL = CreateOutputPathSqlite(InputFile, OutputDirectory)
+    var OutputPathSQL = CreateOutputPathSqlite(InputFile, WriteToDirectory)
     ExtractSqliteFromGFF(InputAsGFF, OutputPathSQL)
 
   #Translate GFF to JSON
@@ -27,7 +34,7 @@ proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool) 
   postProcessJson(OutputJSON)
 
   #Creates path for JSON to be saved to
-  var OutputPath = CreateOutputPathJSON(InputFile, OutputDirectory)
+  var OutputPath = CreateOutputPathJSON(InputFile, WriteToDirectory)
   var OutputStream = openFileStream(OutputPath, fmWrite)
   OutputStream.write(OutputJSON.pretty)
   OutputStream.write("\n")
@@ -37,7 +44,7 @@ proc BICtoJSON*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool) 
   echo "Translation to JSON complete: " & OutputPath
 
 
-proc JSONtoBIC*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool) =
+proc JSONtoBIC*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool = false, ConfigWriteInPlace: bool = false) =
   echo "Attempting translation to BIC: " & InputFile
   #Streams data from InputFile as a string
   var InputStream = newStringStream(readFile(InputFile))
@@ -50,6 +57,7 @@ proc JSONtoBIC*(InputFile: string, OutputDirectory: string, ExpectSqlite: bool) 
     var InputPathSQL = SetFileExtensionSqlite(InputFile)
     if fileExists(InputPathSQL):
       PackSqliteIntoGFF(InputAsGFF, InputPathSQL)
+      echo "Packing SQL into BIC " & InputPathSQL
 
   #Creates path for BIC to be saved to
   var OutputPath = CreateOutputPathBIC(InputFile, OutputDirectory)

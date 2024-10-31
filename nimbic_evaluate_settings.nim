6664acfe9cdbcacb01518c188d64e4423a471365
darkwaffle <darkewaffle@gmail.com>
2024-10-31 00:18:35 -0400

    Code to evaluate Config and CmdLine settings into final SettingsPackage

--- nimbic_evaluate_settings.nim
+++ nimbic_evaluate_settings.nim
@@ -1,11 +1,14 @@
 import nimbic_arguments
 import nimbic_config
+import object_settingspackage
 
 proc ReconcileCommandLineArgumentsAndConfigSettings*()
 proc EvaluateInputDirectory()
 proc EvaluateOutputDirectory()
 proc Evaluate2DADirectory()
 proc EvaluateProduction()
+
+
 
 var ConfigProductionState*: bool
 var ConfigReadSubdirectories*: bool
@@ -40,6 +43,67 @@ proc EvaluateProduction() =
     ConfigOutputJSON = ConfigServerVault
     ConfigReadSubdirectories = true
     ConfigWriteInPlace = true
+
+proc GetOperationSettings(): SettingsPackage =
+    var CommandLineSettings = GetSettingsFromCommandLine()
+    var ConfigFileSettings = GetSettingsPackageFromConfigFile()
+    var ResultSettings: SettingsPackage
+
+    #Set values that can only come from command line.
+    ResultSettings.OperationMode = CommandLineSettings.OperationMode
+    ResultSettings.Race = CommandLineSettings.Race
+    ResultSettings.RaceActive = CommandLineSettings.RaceActive
+    ResultSettings.Subrace = CommandLineSettings.Subrace
+    ResultSettings.SubraceActive = CommandLineSettings.SubraceActive
+    ResultSettings.Class = CommandLineSettings.Class
+    ResultSettings.ClassActive = CommandLineSettings.ClassActive
+    ResultSettings.Level = CommandLineSettings.Level
+    ResultSettings.LevelActive = CommandLineSettings.LevelActive
+    ResultSettings.Feat = CommandLineSettings.Feat
+    ResultSettings.FeatActive = CommandLineSettings.FeatActive
+    ResultSettings.AbilityInput = CommandLineSettings.AbilityInput
+    ResultSettings.HPInput = CommandLineSettings.HPInput
+
+    #Set values that can only come from config.ini
+    ResultSettings.ExpectSqlite = ConfigFileSettings.ExpectSqlite
+    ResultSettings.AutoCleanup = ConfigFileSettings.AutoCleanup
+    ResultSettings.AutoBackup = ConfigFileSettings.AutoBackup
+    ResultSettings.ServerVault = ConfigFileSettings.ServerVault
+
+    #Resolve setting conflicts. If command line input has value then prioritize it. Otherwise use config.ini value.
+    if CommandLineSettings.Input2DA != "":
+        ResultSettings.Input2DA = CommandLineSettings.Input2DA
+    else:
+        ResultSettings.Input2DA = ConfigFileSettings.Input2DA
+
+    if CommandLineSettings.InputBIC != "":
+        ResultSettings.InputBIC = CommandLineSettings.InputBIC
+    else:
+        ResultSettings.InputBIC = ConfigFileSettings.InputBIC
+
+    if CommandLineSettings.OutputJSON != "":
+        ResultSettings.OutputJSON = CommandLineSettings.OutputJSON
+    else:
+        ResultSettings.OutputJSON = ConfigFileSettings.OutputJSON
+
+    if CommandLineSettings.InputJSON != "":
+        ResultSettings.InputJSON = CommandLineSettings.InputJSON
+    else:
+        ResultSettings.InputJSON = ConfigFileSettings.InputJSON
+
+    if CommandLineSettings.OutputBIC != "":
+        ResultSettings.OutputBIC = CommandLineSettings.OutputBIC
+    else:
+        ResultSettings.OutputBIC = ConfigFileSettings.OutputBIC
+
+    #Evaluate if production mode is active. ReadSubDirectories and WriteInPlace only apply to Production operations
+    #on server vaults which contain .bic files spread out into subdirectories.
+    if CommandLineSettings.ProductionState and ConfigFileSettings.ProductionState:
+        ResultSettings.ProductionState = true
+        ResultSettings.ReadSubdirectories = true
+        ResultSettings.WriteInPlace = true
+
+    return ResultSettings
 
 #expose results as Setting vars
 #expose settinghasvalue():bool functions

4dd38115537e49c2220283c79fec404996d94146
darkwaffle <darkewaffle@gmail.com>
2024-11-03 01:09:10 -0500

    Added many new character JSON getter procs

--- character_getters_setters.nim
+++ character_getters_setters.nim
@@ -1,4 +1,4 @@
-import std/json
+import std/[json, strutils]
 
 proc CharacterHasClassLevel*(CharacterJSON: JsonNode, RequiredClass: int, RequiredLevel: int): bool
 proc CharacterHasClass*(CharacterJSON: JsonNode, RequiredClass: int): bool
@@ -73,9 +73,80 @@ proc GetCharacterEpicLevels*(CharacterJSON: JsonNode): int =
         return EpicLevels
 
 
+proc GetCharacterStrength*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["Str"]["value"].getInt
+
+proc GetCharacterDexterity*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["Dex"]["value"].getInt
+
 proc GetCharacterConstitution*(CharacterJSON: JsonNode): int =
     return CharacterJSON["Con"]["value"].getInt
 
-
 proc GetCharacterIntelligence*(CharacterJSON: JsonNode): int =
     return CharacterJSON["Int"]["value"].getInt
+
+proc GetCharacterWisdom*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["Wis"]["value"].getInt
+
+proc GetCharacterCharisma*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["Cha"]["value"].getInt
+
+proc GetCharacterFirstName*(CharacterJSON: JsonNode): string =
+    var FirstName = CharacterJSON["FirstName"]["value"]["0"].getStr
+    removePrefix(FirstName, " ")
+    removeSuffix(FirstName, " ")
+    return FirstName
+
+proc GetCharacterLastName*(CharacterJSON: JsonNode): string =
+    var LastName = CharacterJSON["LastName"]["value"]["0"].getStr
+    removePrefix(LastName, " ")
+    removeSuffix(LastName, " ")
+    return LastName
+
+
+proc GetCharacterFullName*(CharacterJSON: JsonNode): string =
+    var FullName = GetCharacterFirstName(CharacterJSON) & " " & GetCharacterLastName(CharacterJSON)
+    removePrefix(FullName, " ")
+    removeSuffix(FullName, " ")
+    return FullName
+
+proc GetCharacterDeity*(CharacterJSON: JsonNode): string =
+    return CharacterJSON["Deity"]["value"].getStr
+
+proc GetCharacterRace*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["Race"]["value"].getInt
+
+proc GetCharacterSubrace*(CharacterJSON: JsonNode): string =
+    return CharacterJSON["Subrace"]["value"].getStr
+
+proc GetCharacterGoodEvil*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["GoodEvil"]["value"].getInt
+
+proc GetCharacterGoodEvilDescription*(CharacterJSON: JsonNode): string =
+    var AlignValue = GetCharacterGoodEvil(CharacterJSON)
+    if AlignValue > 75:
+        return "Good"
+    elif AlignValue < 25:
+        return "Evil"
+    else:
+        return "Neutral"
+
+proc GetCharacterLawfulChaotic*(CharacterJSON: JsonNode): int =
+    return CharacterJSON["LawfulChaotic"]["value"].getInt
+
+proc GetCharacterLawfulChaoticDescription*(CharacterJSON: JsonNode): string =
+    var AlignValue = GetCharacterLawfulChaotic(CharacterJSON)
+    if AlignValue > 75:
+        return "Lawful"
+    elif AlignValue < 25:
+        return "Chaotic"
+    else:
+        return "Neutral"
+
+proc GetCharacterClasses*(CharacterJSON: JsonNode): seq[array[2, int]] =
+    var ClassesAndLevels: seq[array[2, int]]
+    for i in CharacterJSON["ClassList"]["value"].elems.low .. CharacterJSON["ClassList"]["value"].elems.high:
+        var ClassID = CharacterJSON["ClassList"]["value"][i]["Class"]["value"].getInt
+        var ClassLevel = CharacterJSON["ClassList"]["value"][i]["ClassLevel"]["value"].getInt
+        ClassesAndLevels.add [ClassID, ClassLevel]
+    return ClassesAndLevels

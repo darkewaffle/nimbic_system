74e7256b482f7c62ca8997a90764e447b4a1ea78
darkwaffle <darkewaffle@gmail.com>
2024-11-06 07:29:16 -0500

    More procs to automate HTML formatting/production

--- html_formatting.nim
+++ html_formatting.nim
@@ -7,7 +7,7 @@ proc WrapStyle*(Input: string): string
 proc WrapBody*(Input: string): string
 
 proc WrapDiv*(Input: string, Class: string = ""): string
-proc WrapTable*(Input: string, Class: string = ""): string
+proc WrapTable*(Input: string, Class: string = "", WrapDiv: bool = false): string
 proc WrapTH*(Input: string, Class: string = ""): string
 proc WrapTR*(Input: string, Class: string = ""): string
 proc WrapTD*(Input: string, Class: string = ""): string
@@ -15,6 +15,7 @@ proc WrapTD*(Input: string, Class: string = ""): string
 proc CreateTableHeader*(InputValues: openArray[string], Class: string = "", ClassSuffix: bool = false): string
 proc CreateTableRow*(InputValues: openArray[string], Class: string = ""): string
 proc WidthStyle*(TableClass: string, THClass: string, NumberOfColumns: int): string
+proc MakeTDTable*(TableData: openArray[string], TableClass: string, CellClass: string, TableWidth: int, WrapDiv: bool = false): string
 
 
 proc WrapTags(Input: string, Tag: string, Class: string = ""): string =
@@ -38,8 +39,14 @@ proc WrapStyle*(Input: string): string =
 proc WrapDiv*(Input: string, Class: string = ""): string =
     return WrapTags(Input, "div", Class)
 
-proc WrapTable*(Input: string, Class: string = ""): string =
-    return WrapTags(Input, "table", Class)
+proc WrapSpan*(Input: string, Class: string = ""): string =
+    return WrapTags(Input, "span", Class)
+
+proc WrapTable*(Input: string, Class: string = "", WrapDiv: bool = false): string =
+    if WrapDiv:
+        return WrapDiv(WrapTags(Input, "table", Class), Class)
+    else:
+        return WrapTags(Input, "table", Class)
 
 proc WrapTH*(Input: string, Class: string = ""): string =
     return WrapTags(Input, "th", Class)
@@ -66,18 +73,18 @@ proc CreateTableRow*(InputValues: openArray[string], Class: string = ""): string
     return WrapTR(RowHTML, Class)
 
 proc WidthStyle*(TableClass: string, THClass: string, NumberOfColumns: int): string =
-    var TableWidth = NumberOfColumns * 10
-    if TableWidth < 50:
-        TableWidth = 50
+#    var TableWidth = NumberOfColumns * 10
+#    if TableWidth < 50:
+#        TableWidth = 50
     
     var ColumnWidth = floor(100 / NumberOfColumns).toInt
     
-    var TableStyle= " ." & TableClass & " {width: " & $TableWidth & "%;} "
+#    var TableStyle= " ." & TableClass & " {width: " & $TableWidth & "%;} "
     var THStyle = " ." & THClass & " {width: " & $ColumnWidth & "%;} "
+    return THStyle
+#    return TableStyle & THStyle
 
-    return TableStyle & THStyle
-
-proc MakeTDTable*(TableData: openArray[string], TableClass: string, CellClass: string, TableWidth: int): string =
+proc MakeTDTable*(TableData: openArray[string], TableClass: string, CellClass: string, TableWidth: int, WrapDiv: bool = false): string =
     var
         TableHTML: string
         RowHTML: string
@@ -85,12 +92,39 @@ proc MakeTDTable*(TableData: openArray[string], TableClass: string, CellClass: s
     for i in TableData.low .. TableData.high:
         RowHTML = RowHTML & WrapTD(TableData[i], CellClass)
 
-        if floorMod(i+1, TableWidth) != 0 and i == TableData.high:
-            RowHTML = RowHTML & WrapTD("", CellClass)
+#        if floorMod(i+1, TableWidth) != 0 and i == TableData.high:
+#            RowHTML = RowHTML & WrapTD("", CellClass)
 
         if floorMod(i+1, TableWidth) == 0 or i == TableData.high:
             RowHTML = WrapTR(RowHTML)
             TableHTML = TableHTML & RowHTML
             RowHTML = ""
 
-    return WrapTable(TableHTML, TableClass)
+    return WrapTable(TableHTML, TableClass, WrapDiv)
+
+proc MakeTDTable*(TableData: openArray[int], TableClass: string, CellClass: string, TableWidth: int, WrapDiv: bool = false): string =
+    var
+        TableHTML: string
+        RowHTML: string
+
+    for i in TableData.low .. TableData.high:
+        RowHTML = RowHTML & WrapTD($TableData[i], CellClass)
+
+#        if floorMod(i+1, TableWidth) != 0 and i == TableData.high:
+#            RowHTML = RowHTML & WrapTD("", CellClass)
+
+        if floorMod(i+1, TableWidth) == 0 or i == TableData.high:
+            RowHTML = WrapTR(RowHTML)
+            TableHTML = TableHTML & RowHTML
+            RowHTML = ""
+
+    return WrapTable(TableHTML, TableClass, WrapDiv)
+
+proc MakeStyle(InputPrefix: string, InputName: string, InputStyle: string): string =
+    return InputPrefix & InputName & " " & InputStyle
+
+proc MakeStyleClass*(InputName: string, InputStyle: string): string =
+    return MakeStyle(".", InputName, InputStyle)
+
+proc MakeStyleID*(InputName: string, InputStyle: string): string =
+    return MakeStyle("#", InputName, InputStyle)

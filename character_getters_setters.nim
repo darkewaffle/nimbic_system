bfc6b21901c4d62f01c67c847968ff942edab11a
darkwaffle <darkewaffle@gmail.com>
2024-11-06 01:21:27 -0500

    Support for get spells known by character as IDs or names

--- character_getters_setters.nim
+++ character_getters_setters.nim
@@ -1,4 +1,5 @@
-import std/[json, strutils]
+import std/[algorithm, json, strutils]
+from read2da import GetSpellLabel
 
 proc CharacterHasClassLevel*(CharacterJSON: JsonNode, RequiredClass: int, RequiredLevel: int): bool
 proc CharacterHasClass*(CharacterJSON: JsonNode, RequiredClass: int): bool
@@ -211,3 +212,36 @@ proc GetCharacterAbilityScoresStart*(CharacterJSON: JsonNode): array[6, int] =
         StartingAbilities[i] = CurrentScores[i] - IncreasesFromLevels[i] - IncreasesFromFeats[i]
 
     return StartingAbilities
+
+proc GetCharacterSpellsFromClassAsSpellIDs*(CharacterJSON: JsonNode, ClassID: int): seq[seq[int]] =
+    var SpellsByLevel: seq[seq[int]]
+    for i in CharacterJSON["ClassList"]["value"].elems.low .. CharacterJSON["ClassList"]["value"].elems.high:
+        if CharacterJSON["ClassList"]["value"][i]["Class"]["value"].getInt != ClassID:
+            discard
+        else:
+            for j in 0 .. 9:
+                var SpellsInLevel: seq[int]
+                try:
+                    for k in CharacterJSON["ClassList"]["value"][i]["KnownList" & $j]["value"].elems.low .. CharacterJSON["ClassList"]["value"][i]["KnownList" & $j]["value"].elems.high:
+                        SpellsInLevel.add(CharacterJSON["ClassList"]["value"][i]["KnownList" & $j]["value"][k]["Spell"]["value"].getInt)
+                except KeyError:
+                    break
+                SpellsByLevel.add(SpellsInLevel)
+    return SpellsByLevel
+
+proc GetCharacterSpellsFromClassAsNames*(CharacterJSON: JsonNode, ClassID: int): seq[seq[string]] =
+    var SpellsByLevel: seq[seq[string]]
+    for i in CharacterJSON["ClassList"]["value"].elems.low .. CharacterJSON["ClassList"]["value"].elems.high:
+        if CharacterJSON["ClassList"]["value"][i]["Class"]["value"].getInt != ClassID:
+            discard
+        else:
+            for j in 0 .. 9:
+                var SpellsInLevel: seq[string]
+                try:
+                    for k in CharacterJSON["ClassList"]["value"][i]["KnownList" & $j]["value"].elems.low .. CharacterJSON["ClassList"]["value"][i]["KnownList" & $j]["value"].elems.high:
+                        SpellsInLevel.add(GetSpellLabel(CharacterJSON["ClassList"]["value"][i]["KnownList" & $j]["value"][k]["Spell"]["value"].getInt,true))
+                except KeyError:
+                    break
+                SpellsInLevel.sort()
+                SpellsByLevel.add(SpellsInLevel)
+    return SpellsByLevel

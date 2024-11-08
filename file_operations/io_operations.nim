aedd6850c686b97397759bfdbbf9f39c4869a76e
darkwaffle <darkewaffle@gmail.com>
2024-11-07 20:42:47 -0500

    Spacing for readability

--- file_operations/io_operations.nim
+++ file_operations/io_operations.nim
@@ -1,2 +1,2 @@
 import io_operations/[backups, change_extension, create_path, get_files, io_constants]
-export backups, change_extension, create_path, get_files, io_constants
+export                backups, change_extension, create_path, get_files, io_constants

386d9d6b8bed86798fc34e7515d6b3e95b8cf9bd
darkwaffle <darkewaffle@gmail.com>
2024-11-07 20:41:56 -0500

    Unified import/export files for bic_as_json_operations

---
+++ bic_as_json_operations/interface_change.nim
@@ -0,0 +1,2 @@
+import /[ability_modify, feat_add, feat_remove, hp_max, hp_modify]
+export   ability_modify, feat_add, feat_remove, hp_max, hp_modify
---
+++ bic_as_json_operations/interface_get.nim
@@ -0,0 +1,2 @@
+import get/[abilities, classes, feats, hp, levels, person, race, spells]
+export      abilities, classes, feats, hp, levels, person, race, spells

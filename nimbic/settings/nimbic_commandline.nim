60b6ad3dab3a7e01f741b38c64bd61181a2b9fd8
darkwaffle <darkewaffle@gmail.com>
2024-11-09 00:04:06 -0500

    nimbic_arguments renamed to nimbic_commandline for clarity

--- nimbic/settings/nimbic_commandline.nim
+++ nimbic/settings/nimbic_commandline.nim
@@ -1,48 +1,12 @@
 import std/[os, parseopt, strutils]
-
 import /[object_settingspackage]
 from ../../bic_as_json_operations/operations/ability_modify import AbilityIndex
 
-proc AssignArgumentsToVariables*(CommandLineArguments: var OptParser)
 proc GetSettingsFromCommandLine*(): SettingsPackage
-
-#Variables for input from arguments
-const
-    DefaultMode* = "defaultmode"
-    DefaultRace* = -1
-    DefaultSubrace* = "defaultsubrace"
-    DefaultClass* = -1
-    DefaultLevel* = -1
-    DefaultFeatID* = -1
-    DefaultAbilityInput* = [0, 0, 0, 0, 0, 0]
-    DefaultHPInput* = 0
-var
-    Mode* = DefaultMode
-    RequiredRace* = DefaultRace
-    RequiredSubrace* = DefaultSubrace
-    RequiredClass* = DefaultClass
-    RequiredLevel* = DefaultLevel
-    FeatID* = DefaultFeatID
-    AbilityInput* = DefaultAbilityInput
-    HPInput* = DefaultHPInput
-
-#Variables for input relating to file input/output
-const
-    DefaultInputDirectory* = "defaultinput"
-    DefaultOutputDirectory* = "defaultoutput"
-    DefaultInput2DA* = "default2da"
-    DefaultInputProduction* = false
-var
-    ArgInputDirectory* = DefaultInputDirectory
-    ArgOutputDirectory* = DefaultOutputDirectory
-    ArgInput2DA* = DefaultInput2DA
-    ArgInputProduction* = DefaultInputProduction
-
 
 proc GetSettingsFromCommandLine*(): SettingsPackage =
     var 
         CommandLineArguments = initOptParser(quoteShellCommand(commandLineParams()))
-        #CommandLineSettings: SettingsPackage
         CommandLineSettings = NewSettingsPackage()
 
     for kind, key, val in CommandLineArguments.getopt():
@@ -117,61 +81,3 @@ proc GetSettingsFromCommandLine*(): SettingsPackage =
             else:
                 discard
     return CommandLineSettings
-
-
-proc AssignArgumentsToVariables*(CommandLineArguments: var OptParser) =
-    for kind, key, val in CommandLineArguments.getopt():
-        case kind
-            of cmdArgument:
-                #Not supported. These are arguments not preceded by - or -- and are usually handled by the order in which they are input.
-                discard
-
-            #LongOption are prefixed with --, short option prefixed with -
-            of cmdLongOption, cmdShortOption:
-                case key
-
-                #Operational parameters
-                    of "mode", "m":
-                        Mode = val
-
-                    of "featid", "feat", "f":
-                        FeatID = parseInt(val)
-
-                    of "str", "dex", "con", "int", "wis", "cha":
-                        AbilityInput[AbilityIndex(key)] = parseInt(val)
-
-                    of "hp":
-                        HPInput = parseint(val)
-
-
-                #Character requirements / filters
-                    of "race", "r":
-                        RequiredRace = parseInt(val)
-
-                    of "subrace", "sub", "sr":
-                        RequiredSubrace = val
-
-                    of "class", "c":
-                        RequiredClass = parseInt(val)
-
-                    of "level", "lvl", "l":
-                        RequiredLevel = parseInt(val)
-
-
-                #Input/output parameters
-                    of "input", "id":
-                        ArgInputDirectory = val
-
-                    of "output", "od":
-                        ArgOutputDirectory = val
-
-                    of "input2da", "i2da":
-                        ArgInput2DA = val
-
-                    of "production", "prod":
-                        ArgInputProduction = true
-
-                    else:
-                        discard
-            else:
-                discard

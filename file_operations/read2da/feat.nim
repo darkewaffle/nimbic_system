535db7cdbfe4d8cb553b27b5690fc49349bfa6c7
darkwaffle <darkewaffle@gmail.com>
2024-11-10 11:09:38 -0500

    Implemented GetFeatConstant

--- file_operations/read2da/feat.nim
+++ file_operations/read2da/feat.nim
@@ -6,14 +6,16 @@ const
     FeatFileName = "feat"
     FeatIgnoreLines = 3
     FeatIgnoreColumns = 0
-    FeatReadColumns = 2
+    FeatReadColumns = 36
     FeatColumnFeatID = 0
     FeatColumnFeatLabel = 1
+    FeatColumnFeatConstant = 35
 var
     Feat2DA: seq[seq[string]]
 
 proc Read2DA_Feat*() 
 proc GetFeatLabel*(FeatID: int, Pretty: bool = false): string
+proc GetFeatConstant*(FeatID: int, Pretty: bool = true): string
 
 proc Read2DA_Feat*() =
     Feat2DA = Read2DA(FeatFileName, FeatIgnoreLines, FeatIgnoreColumns, FeatReadColumns)
@@ -25,4 +27,13 @@ proc GetFeatLabel*(FeatID: int, Pretty: bool = false): string =
                 return Feat2DA[i][FeatColumnFeatLabel]
             else:
                 return PrettyString(Feat2DA[i][FeatColumnFeatLabel])
+    return ""
+
+proc GetFeatConstant*(FeatID: int, Pretty: bool = true): string =
+    for i in Feat2DA.low .. Feat2DA.high:
+        if SafeParseInt2DA(Feat2DA[i][FeatColumnFeatID]) == FeatID:
+            if not(Pretty):
+                return Feat2DA[i][FeatColumnFeatConstant]
+            else:
+                return PrettyString(Feat2DA[i][FeatColumnFeatConstant])
     return ""

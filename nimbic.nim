70a12896d45f2f12be9eb47db59ece248e60ad31
darkwaffle <darkewaffle@gmail.com>
2024-11-10 19:54:44 -0500

    Removed deprecated code in comments

--- nimbic.nim
+++ nimbic.nim
@@ -1,23 +1,17 @@
 import std/[dirs, json, os, parseopt, paths]
-
 import nimbic/[echo_feedback, nimbic_help]
 import nimbic/settings/[nimbic_evaluate_settings,object_settingspackage]
-
 import file_operations/[interface_2da, interface_conversion, interface_io]
 import bic_as_json_operations/[interface_get, interface_operations]
 
-#proc PerformModeOperation()
-proc PerformModeOperationFromPackage()
-#proc ValidateModeArguments()
-proc ValidateModeArgumentsFromPackage()
-#proc EvaluateCharacterRequirements(CharacterJSON: JsonNode, CharacterFileLocation: string): bool
+proc PerformModeOperation()
+proc ValidateSettings()
 proc EvaluateCharacterRequirementsFromPackage(CharacterJSON: JsonNode, CharacterFileLocation: string): bool
 proc GetCore2DAFiles(OperationSettings: SettingsPackage)
 
 
 #Operational variables
 var
-#    CommandLineArguments = initOptParser(quoteShellCommand(commandLineParams()))
     MeetsRequirements = false
     FilesToChange: seq[Path]
     CharacterJSON: JsonNode
@@ -33,21 +27,16 @@ const
     ModeRequiresClassAndLevel = ["addclassfeat", "removeclassfeat"]
 
 
-#####
-#Program begins here
-#####
+## ## ## ## ## ## ## ## ##
+##   Code begins here   ##
+## ## ## ## ## ## ## ## ##
 EchoSeparator()
 OperationSettings = GetOperationSettings()
 OperationSettings.EchoSettings()
-#GetSettingsFromConfigFile()
-#AssignArgumentsToVariables(CommandLineArguments)
-#ReconcileCommandLineArgumentsAndConfigSettings()
-#ValidateModeArguments()
-ValidateModeArgumentsFromPackage()
-#PerformModeOperation()
-PerformModeOperationFromPackage()
+ValidateSettings()
+PerformModeOperation()
 
-proc ValidateModeArgumentsFromPackage() =
+proc ValidateSettings() =
     if not(OperationSettings.Mode in ValidModes):
         echo "Invalid --mode argument specified."
         quit(QuitSuccess)
@@ -116,7 +105,7 @@ proc ValidateModeArgumentsFromPackage() =
                 #No specific requirements.
                 discard
 
-proc PerformModeOperationFromPackage() =
+proc PerformModeOperation() =
     if OperationSettings.Mode in ModeNoOperation:
         case OperationSettings.Mode:
             of "help":
@@ -226,162 +215,6 @@ proc EvaluateCharacterRequirementsFromPackage(CharacterJSON: JsonNode, Character
         EchoMessageNameFilename("Operation requirements not met, " & OperationSettings.Mode & " will be skipped.", CharacterJSON, CharacterFileLocation)
     return RequirementResult
 
-#[
-proc ValidateModeArguments() =
-    if not(Mode in ValidModes):
-        echo "Invalid --mode argument specified."
-        quit(QuitSuccess)
-
-    if Mode in ModeFileOperations:
-        case Mode:
-            of "bictojson":
-                if not(dirExists(Path ConfigInputBIC)):
-                    EchoError("Directory is not valid - " & $ConfigInputBic)
-                    quit(QuitSuccess)
-
-            of "jsontobic":
-                if not(dirExists(Path ConfigInputJSON)):
-                    EchoError("Directory is not valid - " & $ConfigInputJSON)
-                    quit(QuitSuccess)
-
-    if Mode in ModeCharacterModify:
-        if Mode in ModeRequires2DA:
-            if not(dirExists(Path ConfigInput2DA)):
-                EchoError("Directory is not valid - " & $ConfigInput2DA)
-                quit(QuitSuccess)
-
-        if not(dirExists(Path ConfigInputJSON)):
-            EchoError("Directory is not valid - " & $ConfigInputJSON)
-            quit(QuitSuccess)
-
-        case Mode:
-            of "addclassfeat", "removeclassfeat":
-                if (RequiredClass == DefaultClass) or (FeatID == DefaultFeatID) or (RequiredLevel == DefaultLevel):
-                    EchoError("A class, feat and level must be input.")
-                    quit(QuitSuccess)
-
-            of "alterclasshp":
-                if RequiredClass == DefaultClass:
-                    EchoError("A class must be input.")
-                    quit(QuitSuccess)
-
-            of "maxhp":
-                #No specific requirements.
-                discard
-
-            of "addfeat":
-                if FeatID == DefaultFeatID:
-                    EchoError("A feat ID must be input.")
-                    quit(QuitSuccess)
-
-            of "removefeat":
-                if FeatID == DefaultFeatID:
-                    EchoError("A feat ID must be input.")
-                    quit(QuitSuccess)
-
-            of "modifyability":
-                #No specific requirements.
-                discard
-
-
-proc PerformModeOperation() =
-    if Mode in ModeNoOperation:
-        case Mode:
-            of "help":
-                EchoBlank()
-                DisplayHelp()
-                quit(QuitSuccess)
-
-    if Mode in ModeFileOperations:
-        case Mode:
-            of "bictojson":
-                FilesToChange = GetBICFiles(ConfigInputBIC, ConfigReadSubdirectories)
-                for i in FilesToChange.low .. FilesToChange.high:
-                    BICtoJSON(FilesToChange[i], ConfigOutputJSON, ConfigSqlite, ConfigWriteInPlace)
-
-            of "jsontobic":
-                FilesToChange = GetJSONFiles(ConfigInputJSON, ConfigReadSubdirectories)
-                for i in FilesToChange.low .. FilesToChange.high:
-                    JSONtoBIC(FilesToChange[i], ConfigOutputBIC, ConfigSqlite, ConfigWriteInPlace)
-
-    if Mode in ModeCharacterModify:
-        if Mode in ModeRequires2DA:
-            GetCore2DAFiles()
-
-        FilesToChange = GetJSONFiles(ConfigInputJSON, ConfigReadSubdirectories)
-        for i in FilesToChange.low .. FilesToChange.high:
-            EchoBlank()
-            CharacterJSON = parseFile(FilesToChange[i])
-            MeetsRequirements = EvaluateCharacterRequirements(CharacterJSON, FilesToChange[i])
-
-            if not(MeetsRequirements):
-                discard
-            else:
-                case Mode:
-                    of "addclassfeat":
-                        AddClassFeat(CharacterJSON, RequiredClass, RequiredLevel, FeatID)
-
-                    of "removeclassfeat":
-                        RemovalSuccessful = RemoveClassFeat(CharacterJSON, RequiredClass, RequiredLevel, FeatID)
-                        MeetsRequirements = MeetsRequirements and RemovalSuccessful
-
-                    of "alterclasshp":
-                        ModificationSuccessful = AlterClassHP(CharacterJSON, RequiredClass, HPInput)
-                        MeetsRequirements = MeetsRequirements and ModificationSuccessful
-
-                    of "maxhp":
-                        ModificationSuccessful = MaximizeHP(CharacterJSON)
-                        MeetsRequirements = MeetsRequirements and ModificationSuccessful
-
-                    of "addfeat":
-                        AddLevelFeat(CharacterJSON, IfNoLevelThen1(RequiredLevel), FeatID)
-
-                    of "removefeat":
-                        RemovalSuccessful = RemoveLevelFeat(CharacterJSON, IfNoLevelThen1(RequiredLevel), FeatID)
-                        MeetsRequirements = MeetsRequirements and RemovalSuccessful
-
-                    of "modifyability":
-                        ModifyAbilities(CharacterJSON, AbilityInput)
-
-            if MeetsRequirements:
-                writeFile(FilesToChange[i], pretty(CharacterJSON, 4))
-                echo "Writing to " & FilesToChange[i]
-
-proc EvaluateCharacterRequirements(CharacterJSON: JsonNode, CharacterFileLocation: string): bool =
-    var
-        RequirementRace = true
-        RequirementSubrace = true
-        RequirementClass = true
-        RequirementClassLevel = true
-        RequirementTotalLevel = true
-        RequirementResult: bool
-
-    if RequiredRace != DefaultRace:
-        RequirementRace = RequirementRace and CharacterHasRace(CharacterJSON, RequiredRace)
-        #echo "Has required race = " & $RequirementRace
-
-    if RequiredSubrace != DefaultSubrace:
-        RequirementSubrace = RequirementSubrace and CharacterHasSubrace(CharacterJSON, RequiredSubrace)
-        #echo "Has required subrace = " & $RequirementSubrace
-
-    if Mode == "addclassfeat" or Mode == "removeclassfeat":
-        RequirementClassLevel = RequirementClassLevel and CharacterHasClassLevel(CharacterJSON, RequiredClass, RequiredLevel)
-        #echo "Has required class level = " & $RequirementClassLevel
-    else:
-        if RequiredLevel != DefaultLevel:
-            RequirementTotalLevel = RequirementTotalLevel and CharacterHasTotalLevel(CharacterJSON, RequiredLevel)
-            #echo "Has required total level = " & $RequirementTotalLevel
-        if RequiredClass != DefaultClass:
-            RequirementClass = RequirementClass and CharacterHasClass(CharacterJSON, RequiredClass)
-            #echo "Has required class = " & $RequirementClass
-
-    RequirementResult = RequirementRace and RequirementSubrace and RequirementClass and RequirementClassLevel and RequirementTotalLevel
-    if RequirementResult:
-        EchoMessageNameFilename("Operation requirements met, " & Mode & " will begin.", CharacterJSON, CharacterFileLocation)
-    else:
-        EchoMessageNameFilename("Operation requirements not met, " & Mode & " will be skipped.", CharacterJSON, CharacterFileLocation)
-    return RequirementResult
-]#
 proc GetCore2DAFiles(OperationSettings: SettingsPackage) =
     Initialize2DAs(OperationSettings)
     InitializeRDDHPLookup()
